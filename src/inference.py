1d636c7f99b1e404a3b81e12c6b8f18131b2bad3
AyaMYoussef <35687458+AyaMYousef@users.noreply.github.com>
2025-08-17 11:56:26 +0300

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -8,20 +8,17 @@ def predict_survival(passengers: list[PassengerData]):
     base_model=[p.model_dump() for p in passengers]
 
     for i,p in enumerate(passengers):
-        base_model[i]["family_size"] = p.family_size
-        base_model[i]["is_alone"] = p.is_alone
-        base_model[i]["is_child"] =p.is_child
+        """
+        Feature Engineering Params
+        """
 
     df = pd.DataFrame(base_model)
     df_processed = preprocessor.transform(df)
     prediction = (model.predict(df_processed) >0.5).astype("int32")
 
     pred_response = PredictionResponse(prediction=[
-        PassengerPrediction(
-            passenger_id= passenger.passenger_id,
-            predicted = "survived" if pred==1 else "not survived"
-        )
-        for passenger, pred in zip(passengers, prediction.flatten())
+       """"
+       """"
     ])
 
     return pred_response
